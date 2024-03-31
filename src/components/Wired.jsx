9404ff038d30628734a5d0ad996fe5657b8d5c44
equestriann <x.dormostuk@gmail.com>
2024-03-31 19:22:36 +0300

    card structure

--- src/components/Wired.jsx
+++ src/components/Wired.jsx
@@ -4,13 +4,15 @@ export default function Wired(props) {
             <div className="good">
                 <img src={props.good.picture} alt=""/>
                 <div className="good-details">
-                    <p>{props.good.title}</p>
+                    <p>
+                        {props.good.title}
+                        <span className="good-price">{props.good.price}₽</span>
+                    </p>
                     <p>
                         <span className="star">&#9733;</span>
                         {props.good.rating}
+                        <button className="buy-button">Купить</button>
                     </p>
-                    <p>{props.good.price}₽</p>
-                    <button className="buy-button">Купить</button>
                 </div>
             </div>
         </div>
