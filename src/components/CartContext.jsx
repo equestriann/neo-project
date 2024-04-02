414dc0dd1c3db2529d4c5bb203b5aaf0442d6454
equestriann <x.dormostuk@gmail.com>
2024-04-03 00:38:28 +0300

    addToCart in CartContext remade with check for item presence in cart

--- src/components/CartContext.jsx
+++ src/components/CartContext.jsx
@@ -12,7 +12,18 @@ export const CartProvider = ({ children }) => {
     const [cart, setCart] = useState([])
 
     const addToCart = (item) => {
-        setCart([...cart, item])
+        const existingItem = cart.find(cartItem => cartItem.id === item.id)
+        if (existingItem) {
+            const updatedCart = cart.map(cartItem => {
+                if (cartItem.id === item.id) {
+                    return {...cartItem, quantity: cartItem.quantity + 1}
+                }
+                return cartItem
+            })
+            setCart(updatedCart)
+        } else {
+            setCart([...cart, {...item, quantity:1}])
+        }
     }
 
     return (
