5358ec6fcd72c6e03168cb2290ba43149f6303d4
equestriann <x.dormostuk@gmail.com>
2024-04-03 00:53:12 +0300

    goods info output draft

--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -2,14 +2,23 @@ import {useCart} from "../components/CartContext.jsx";
 
 export default function Cart () {
 
-    // const {cart} = useCart()
+    const {cart} = useCart()
     const {totalQuantity} = useCart()
 
     return (
         <div className="wrapper">
-            <div style={{textAlign:"center"}}>
+            <div>
                 Количество товаров в корзине: {totalQuantity}
                 <hr />
+                <div>
+                    {cart.map(good =>
+                        <div key={good.id}>
+                            <p>{good.title}</p>
+                            <p>{good.price}</p>
+                            <p>{good.quantity}</p>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
