358bc6f07111858ad165a1146756224cdcb89d9d
equestriann <x.dormostuk@gmail.com>
2024-04-06 23:21:29 +0300

    cart items card done

--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -1,4 +1,5 @@
 import {useCart} from "../components/CartContext.jsx";
+import { RiDeleteBin2Line } from "react-icons/ri";
 
 export default function Cart () {
 
@@ -23,33 +24,36 @@ export default function Cart () {
 
     return (
         <div>
-            {totalQuantity ?
-                <h3>Корзина</h3> :
-                <h3 style={{textAlign:"center"}}>Корзина пуста</h3>}
-            <div className="cart-content">
-                <div className="cart-item-card">
+            {!totalQuantity ?
+            <h3 style={{textAlign:"center"}}>Корзина пуста</h3> :
+            <h3>Корзина</h3>
+            }
+            <div className="cart-container">
+                <div className="cart-list">
                     {cart.map(good =>
-                        <div key={good.id} className="cart-item-info">
-                            <button onClick={() => deleteGood(good)} style={{fontSize: "30px"}}>&#128465;</button>
-                            <img className="cart-item-image" src={good.img} alt=""/>
-                            <p>{good.title}</p>
-                            <p className="cart-item-price-small">{good.price}₽</p>
-                            <span className="cart-item-quantity" onClick={() => increment(good)}
-                                  style={{fontSize: "30px"}}>+</span>
-                            <span>{good.quantity}</span>
-                            <span className="cart-item-quantity" onClick={() => decrement(good)}
-                                  style={{fontSize: "30px"}}>-</span>
-                            <span className="cart-item-price-main">{good.price}₽</span>
+                        <div key={good.id} className="item-card">
+                            <div className="img-n-quant">
+                                <img src={good.picture} alt="Product image"/>
+                                <div className="quant-group">
+                                    <button id="quant-btn" onClick={() => decrement(good)}>-</button>
+                                    <span>{good.quantity}</span>
+                                    <button id="quant-btn" onClick={() => increment(good)}>+</button>
+                                </div>
+                            </div>
+                            <div className="product-info">
+                                <p>{good.title}</p>
+                                <p id="price-small">{good.price} ₽</p>
+                            </div>
+                            <div className="delete-n-price">
+                                <RiDeleteBin2Line id="item-delete-btn" onClick={() => deleteGood(good)}/>
+                                <p>{good.price} ₽</p>
+                            </div>
                         </div>
                     )}
                 </div>
                 {!totalQuantity ? null :
-                    <div className="total-card">
-                        <span className="total-price">ИТОГО</span>
-                        <span className="total-price">₽{totalPrice}</span>
-                        <p>
-                            <button className="order-button">Перейти к оформлению</button>
-                        </p>
+                    <div className="total-container">
+                        Here's gonna be oder sumup
                     </div>
                 }
             </div>
